db3c46564d1422979f3d5299438b198bb0d93994
Leonard <leonard@mimisiku.dev>
2022-08-14 13:44:54 +0200

    add sonic

--- src/logger.ts
+++ src/logger.ts
@@ -1,7 +1,8 @@
 import pino from 'pino';
+import SonicBoom from 'sonic-boom';
 import config from './config';
 
 export default pino({
     name: config.name,
     level: config.verbose ? 'trace' : 'info',
-});
+}, new SonicBoom({ fd: process.stdout.fd }));
